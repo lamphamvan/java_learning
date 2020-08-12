4fcd661a44a812d48074ab5626ef3f92030b8291
Pham Van Lam <phamlam.nd@gmail.com>
2020-08-12 13:54:23 +0700

    Variable in Java

---
+++ Variable/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		String name;
+		float height;
+		int age;
+		name = "Pham Van Lam";
+		height = 1.7f;
+		age = 30;
+		System.out.println(name);
+		System.out.println(height);
+		System.out.println(age);
+	}
+}
