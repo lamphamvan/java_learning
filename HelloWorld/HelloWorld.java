b316436b0e568c07ecfeb85b9814b415241f46be
Pham Van Lam <phamlam.nd@gmail.com>
2020-08-12 11:25:12 +0700

    Add first project

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+    public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
+
+/*How to run
+* Open cmd
+* javac HelloJava.java
+* java HelloJava
+*/
